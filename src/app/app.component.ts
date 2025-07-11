6cd88e079d42165d400998b800c2365e2aa773d2
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2025-07-11 15:29:25 -0300

    fix: corrige problema da barra de rolagem ao navegar entre páginas
    - Adiciona scrollPositionRestoration: 'top' no RouterModule
    - Implementa listener para NavigationEnd que força scroll para o topo
    - Resolve issue onde a posição da rolagem era mantida ao navegar entre rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'invmetro-site';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      window.scrollTo(0, 0);
+    });
+  }
 }
