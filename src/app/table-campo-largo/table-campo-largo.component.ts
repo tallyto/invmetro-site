2390bb2f520cd60fb025cc7fa78227f752b09a2f
Tállyto <rodrigues.tallyto@hotmail.com>
2026-01-06 11:36:39 -0300

    feat: atualização tabela de preços

--- src/app/table-campo-largo/table-campo-largo.component.ts
+++ src/app/table-campo-largo/table-campo-largo.component.ts
@@ -7,29 +7,35 @@ import { Component } from '@angular/core';
 })
 export class TableCampoLargoComponent {
   veiculosData = [
-    { veiculo: 'Automóvel', gnv: 'R$ 440,00', sinistro: 'R$ 580,00', artesanal: 'R$ 2.500,00', antt: 'X' },
-    { veiculo: 'Automóvel Rebaixado', gnv: 'R$ 440,00', sinistro: 'R$ 580,00', artesanal: 'X', antt: 'X' },
-    { veiculo: 'Veículo Blindado', gnv: 'R$ 440,00', sinistro: 'R$ 900,00', artesanal: 'X', antt: 'X' },
-    { veiculo: 'Utilitário/camioneta/caminhonete', gnv: 'R$ 440,00', sinistro: 'R$ 630,00', artesanal: 'R$ 2.500,00', antt: 'X' },
-    { veiculo: 'Microônibus', gnv: 'R$ 440,00', sinistro: 'R$ 700,00', artesanal: 'X', antt: 'R$ 530,00' },
-    { veiculo: 'Ônibus', gnv: 'R$ 1000,00', sinistro: 'R$ 1000,00', artesanal: 'X', antt: 'R$ 600,00' },
-    { veiculo: 'Caminhão', gnv: 'R$ 1000,00', sinistro: 'R$ 1000,00', artesanal: 'X', antt: 'X' },
-    { veiculo: 'Reboque/Semi-reboque', gnv: 'X', sinistro: 'R$ 1150,00', artesanal: 'X', antt: 'X' },
-    { veiculo: 'Motocicleta/motoneta/ciclomotor até 300 cc', gnv: 'X', sinistro: 'R$ 500,00', artesanal: 'R$ 2.500,00', antt: 'X' },
-    { veiculo: 'Motocicleta/motoneta/ciclomotor acima de 300 cc', gnv: 'X', sinistro: 'R$ 650,00', artesanal: 'R$ 2.500,00', antt: 'X' },
-    { veiculo: 'Triciclo/Quadriciclo', gnv: 'X', sinistro: 'R$ 1000,00', artesanal: 'R$ 2.500,00', antt: 'X' }
+    { veiculo: 'Automóvel', gnv: 'R$ 450,00', sinistro: 'R$ 630,00', artesanal: 'R$ 2.500,00', antt: 'X' },
+    { veiculo: 'Automóvel Rebaixado', gnv: 'R$ 450,00', sinistro: 'R$ 630,00', artesanal: 'X', antt: 'X' },
+    { veiculo: 'Veículo Blindado', gnv: 'R$ 450,00', sinistro: 'R$ 1.000,00', artesanal: 'X', antt: 'X' },
+    { veiculo: 'Utilitário/camioneta/caminhonete', gnv: 'R$ 450,00', sinistro: 'R$ 700,00', artesanal: 'R$ 2.500,00', antt: 'X' },
+    { veiculo: 'Microônibus', gnv: 'R$ 450,00', sinistro: 'R$ 770,00', artesanal: 'X', antt: 'R$ 600,00' },
+    { veiculo: 'Ônibus', gnv: 'R$ 1.100,00', sinistro: 'R$ 1.100,00', artesanal: 'X', antt: 'R$ 650,00' },
+    { veiculo: 'Caminhão', gnv: 'R$ 1.100,00', sinistro: 'R$ 1.100,00', artesanal: 'X', antt: 'X' },
+    { veiculo: 'Reboque/Semi-reboque', gnv: 'R$ 1.300,00', sinistro: 'R$ 2.200,00', artesanal: 'X', antt: 'X' },
+    { veiculo: 'Motocicleta/motoneta/ciclomotor até 300 cc', gnv: 'X', sinistro: 'R$ 550,00', artesanal: 'R$ 2.500,00', antt: 'X' },
+    { veiculo: 'Motocicleta/motoneta/ciclomotor acima de 300 cc', gnv: 'X', sinistro: 'R$ 700,00', artesanal: 'R$ 2.500,00', antt: 'X' },
+    { veiculo: 'Triciclo/Quadriciclo', gnv: 'X', sinistro: 'R$ 1.300,00', artesanal: 'R$ 2.500,00', antt: 'X' }
   ];
 
   servicosData = [
     { servico: 'Laudo Escolar', valor: 'R$ 500,00' },
     { servico: 'Laudo Seguradora/URBS (Taxi)', valor: 'Preço Sinistro' },
-    { servico: 'Laudo Opacidade + Ruído', valor: 'R$ 180,00' },
+    { servico: 'Laudo Opacidade + Ruído', valor: 'R$ 200,00' },
     { servico: 'Basculante Periódico (Res 859/21 Contran)', valor: 'R$ 550,00' },
-    { servico: 'Laudo Ônibus COMEC', valor: 'R$ 480,00' },
-    { servico: 'Motor-Casa', valor: 'R$ 1.950,00' },
-    { servico: 'CITV Mercosul', valor: 'R$ 500,00' },
-    { servico: 'Ensaio de Torção', valor: 'R$ 500,00' },
-    { servico: '4º Eixo em semi-reboque', valor: 'R$ 1.950,00' },
-    { servico: 'Retirada de GNV', valor: 'R$ 700,00' }
+    { servico: 'Laudo Ônibus COMEC', valor: 'R$ 500,00' },
+    { servico: 'Motor-Casa', valor: 'R$ 2.000,00' },
+    { servico: 'CITV Mercosul', valor: 'R$ 550,00' },
+    { servico: 'Ensaio de Torção', valor: 'R$ 600,00' },
+    { servico: '4º Eixo em semi-reboque', valor: 'R$ 2.200,00' },
+    { servico: 'Retirada de GNV', valor: 'R$ 750,00' },
+    { servico: 'Volumétrica (Incluso Art)', valor: 'R$ 650,00' },
+    { servico: 'Teste de Carga (Caminhão)', valor: 'R$ 980,00' },
+    { servico: 'Teste de Carga (Guindaste)', valor: 'R$ 1890,00' },
+    { servico: 'Laudo Quinta Roda', valor: 'R$ 600,00' },
+    { servico: 'Ensaio Torção', valor: 'R$ 600,00' },
+
   ];
 }
